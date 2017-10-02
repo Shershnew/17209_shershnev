32f39fb3411abb5a41e1d04fcc29073d8de9cb6d
shershnev <a.shershnev1@g.nsu.ru>
2017-10-02 16:08:25 +0700

    langton loop

--- lab_3/life.c
+++ lab_3/life.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
 void draw(char * map,int width, int height){
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
         {
-            if(*(map+j+(i * width)) == 1)
-            printf("*");
-            else
+            if(*(map+j+(i * width)) == 0)
                 printf(" ");
+            else
+            printf("%d",*(map+j+(i * width)));
         }
     printf("\n");
     }
@@ -25,56 +26,83 @@ int check(int x,int y,int width,int height){
     return x + (y * width);
 }
 
-void turn(char *map, int width, int height){
-    int count = 0;
-    char *counts = calloc(width * height, 1);
+void turn(char *map, int width, int height,char **rule){
+    char *map2 = calloc(width * height, 1);
+    char tec[7];
+    tec[6] = '\0';
     for (int j = 0; j < height; ++j)
     {
         for (int i = 0; i < width; ++i)
         {
-            count = 0;
-            if(*(map + check(i-1,j-1,width,height)) == 1) count++;
-            if(*(map + check(i-1,j,width,height)) == 1) count++;
-            if(*(map + check(i-1,j+1,width,height)) == 1) count++;
-            if(*(map + check(i,j-1,width,height)) == 1) count++;
-            if(*(map + check(i,j+1,width,height)) == 1) count++;
-            if(*(map + check(i+1,j-1,width,height)) == 1) count++;
-            if(*(map + check(i+1,j,width,height)) == 1) count++;
-            if(*(map + check(i+1,j+1,width,height)) == 1) count++;
-            counts[i+(j*width)] = count;
+            tec[0] = map[check(i,j,width,height)];
+            tec[1] = map[check(i,j-1,width,height)];
+            tec[2] = map[check(i+1,j,width,height)];
+            tec[3] = map[check(i,j+1,width,height)];
+            tec[4] = map[check(i-1,j,width,height)];
+            for (int k = 0; k < 219; ++k)
+            {
+                if(*(rule[k]) - '0' == tec[0] && *(rule[k]+1) - '0' == tec[1] && *(rule[k]+2) - '0' == tec[2] && *(rule[k]+3) - '0' == tec[3] && *(rule[k]+4) - '0' == tec[4]){
+                    map2[i+(j*width)] =*(rule[k]+5) - '0';
+                    break;
+                } else
+                if(*(rule[k]) - '0' == tec[0] && *(rule[k]+1) - '0' == tec[4] && *(rule[k]+2) - '0' == tec[1] && *(rule[k]+3) - '0' == tec[2] && *(rule[k]+4) - '0' == tec[3]){
+                    map2[i+(j*width)] =*(rule[k]+5) - '0';
+                    break;
+                } else
+                if(*(rule[k]) - '0' == tec[0] && *(rule[k]+1) - '0' == tec[3] && *(rule[k]+2) - '0' == tec[4] && *(rule[k]+3) - '0' == tec[1] && *(rule[k]+4) - '0' == tec[2]){
+                    map2[i+(j*width)] =*(rule[k]+5) - '0';
+                    break;
+                } else
+                if(*(rule[k]) - '0' == tec[0] && *(rule[k]+1) - '0' == tec[2] && *(rule[k]+2) - '0' == tec[3] && *(rule[k]+3) - '0' == tec[4] && *(rule[k]+4) - '0' == tec[1]){
+                    map2[i+(j*width)] =*(rule[k]+5) - '0';
+                    break;
+                }
+            }
         }
     }
-    for (int j = 0; j < height; ++j)
+    for (int i = 0; i < width * height; ++i)
     {
-        for (int i = 0; i < width; ++i)
-        {
-            if(*(map + i + (j * width)) == 1 && (counts[i+(j*width)] < 2 || counts[i+(j*width)] > 3))
-                *(map + i + (j * width)) = 0;
-            if(*(map + i + (j * width)) == 0 && counts[i+(j*width)] == 3)
-                *(map + i + (j * width)) = 1;
-        }
+        map[i] = map2[i];
     }
 }
 
 int main(int argc, char **argv){
 
     FILE *file;
+    char *rule[219];
     int width;
     int height;
-    int x,y;
-    file = fopen(argv[1], "r");
+    int x = 1 ,y = 1;
+    int i,j;
+
+    file = fopen("rule.txt", "r");
     fscanf(file,"#Size %d %d\n",&width, &height);
+
     char *map = (char *)calloc(width * height,1);
-    while(!feof(file)){
-        fscanf(file,"%d %d\n",&x, &y);
-        *(map+x-1+((y-1) * width)) = 1;
-   }
+    char str[width];
+    for (i = 0; i < 10; ++i)
+    {
+        for (j = 0; j < width; ++j)
+        {
+            str[j] = 0;
+        }
+        fscanf(file,"%s",str);
+        for (j = 0; j < width; ++j)
+        {
+            if(str[j] != 0)
+            map[j+x+((i+y)*width)] = str[j] - '0';
+        }
+    }
+    for (i = 0; i < 219; ++i)
+    {
+        rule[i] = (char *)calloc(7,1);
+        fscanf(file,"%s",rule[i]);
+    }
     fclose(file);
-    draw(map, width, height);
     while(getch() != 'k'){
-        system("cls");
-        draw(map, width, height);
-        turn(map, width, height);
+    system("cls");
+    draw(map, width, height);
+    turn(map,width,height,rule);
     }
     return 0;
 }
