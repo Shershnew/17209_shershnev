4c73da3887759ef9ca528aa43fc768fc92e9efdb
3000cfyz <3000cfyz@gmail.com>
2017-11-01 17:37:13 +0300

    fix rle reading

--- lab_3/life.c
+++ lab_3/life.c
@@ -5,63 +5,58 @@
 #include <conio.h>
 #include <Windows.h>
 #endif
+#define Size_rule 77778
 
 void draw(char *map, int width, int height){
     int i = 0;
     int j = 0;
     int r = 0;
-    #ifdef _WIN32
+#ifdef _WIN32
     int lastchar = 0;
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    #endif
+#endif
     for (i = 0; i < height; ++i)
     {
         for (j = 0; j < width; ++j)
         {
             r = *(map+j+(i * width));
-            if(r == 0){
-                #ifdef _WIN32
-                if(lastchar != 1){
+            if(0 == r){
+#ifdef _WIN32
+                if(1 != lastchar){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 0));
                     lastchar = 1;
                 }
-                #endif
+#endif
                 printf(" ");
             }
             else{
-                #ifdef _WIN32
+#ifdef _WIN32
                 lastchar = 0;
-                if(r == 1){ 
+                if(1 == r){ 
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 9));
                 }
-                else
-                if(r == 2){
+                else if(2 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 4));
                 }
-                else
-                if(r == 3){
+                else if(3 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 3));
                 }
-                else
-                if(r == 4){
+                else if(4 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 2));
                 }
-                else
-                if(r == 5){
+                else if(5 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 5));
                 }
-                else
-                if(r == 6){
+                else if(6 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 6)); 
                 }
-                else
-                if(r == 7){
+                else if(7 == r){
                     SetConsoleTextAttribute(hConsole, (WORD) ((0 << 4) | 7));
                 }
                 printf("*");
-                #else
+#else
                 printf("%d", r);
-                #endif
+#endif
             }
         }
         printf("\n");
@@ -87,11 +82,10 @@ int check(int x, int y, int width, int height){
 
 void turn(char **map, int width, int height, int *rule){
     char *map2 = calloc(width * height, 1);
-    int i = 0;
-    int j = 0;
-    int k = 0;
     int id = 0;
     char tec[5];
+    int i = 0;
+    int j = 0;
     for (j = 0; j < height; ++j)
     {
         for (i = 0; i < width; ++i)
@@ -102,12 +96,13 @@ void turn(char **map, int width, int height, int *rule){
             tec[3] = (*map)[check(i,j+1,width,height)];
             tec[4] = (*map)[check(i-1,j,width,height)];
             id = 0;
+            int k = 0;
             for (k = 0; k < 5; k++){
-            id *= 10;
-            id += (int)tec[k];
+                id *= 10;
+                id += (int)tec[k];
             }
             if(rule[id] != 0){
-                map2[i+(j*width)] = rule[id]-1;
+                map2[i + (j * width)] = rule[id] - 1;
             }
         }
     }
@@ -127,20 +122,25 @@ char* read_rle(int *width, int *height, char **argv){
     int i = 0;
     int j = 0;
     int count = 0;
+    int x_flag = 0;
     char c = ' ';
     char *map;
     char pointer;
     do{
-        if(fscanf(fileRLE,"%c",&pointer) == -1){
+        if(-1 == fscanf(fileRLE,"%c",&pointer)){
             break;
         }
-        if(pointer == 'x'){
+        if(pointer == 'x' && x_flag == 0){
+            x_flag = 1;
             fscanf(fileRLE," = %d, y = %d", width, height);
             while (c != '\n'){
-                fscanf(fileRLE,"%c",&c);
+                fscanf(fileRLE, "%c", &c);
             }
             map = (char *)calloc(*width * *height, 1);
             fscanf(fileRLE, "%c", &pointer);
+        }
+        if(pointer == '\n' || pointer == '!'){
+            continue;
         }
         if(pointer == '#'){
             while(pointer != '\n'){
@@ -151,9 +151,8 @@ char* read_rle(int *width, int *height, char **argv){
             count *= 10;
             count += pointer - '0';
         }
-        else
-        if(pointer == '.'){
-            if(count == 0){
+        else if(pointer == '.'){
+            if(0 == count){
                 x++;
                 count = 0;
             }
@@ -162,28 +161,32 @@ char* read_rle(int *width, int *height, char **argv){
                 count = 0;
             }
         }
-        else
-        if(pointer == '$'){
+        else if(pointer == '$'){
             y++;
             x = 0;
         }
-        else
-        if(pointer == '\n' || pointer == '!'){
-            continue;
-        }
-        else
-        if(count == 0){
-            map[x+(y * *width)] = pointer-'A'+1;
-            x++;
-            }
-        else{
-            for (j = 0; j < count; ++j){
+        else if(0 == count && (pointer >= 'A' && pointer <= 'G')){
+            if(x+(y * *width) < *width * *height){
                 map[x+(y * *width)] = pointer-'A'+1;
-                x++;
+                x++;    
+            }
+            else{
+                return (char*)1;   
+            }
+        }
+        else if(0 != count && (pointer >= 'A' && pointer <= 'G')){
+            for (j = 0; j < count; ++j){
+                if(x+(y * *width) < *width * *height){
+                    map[x+(y * *width)] = pointer-'A'+1;
+                    x++;    
+                }
+                else{
+                    return (char*)1;
+                }
             }
             count = 0;
         }
-    }while(pointer != '!');
+    } while(pointer != '!');
     fclose(fileRLE);
     return map;
 }
@@ -193,7 +196,7 @@ int* read_rule(char **argv){
     if(NULL == fileRULE){
         return 0;
     }
-    int* rule = (int*)calloc(77778, sizeof(int));
+    int* rule = (int*)calloc(Size_rule, sizeof(int));
     char* str = (char *)calloc(7,1);
     int id = 0;
     int i = 0;
@@ -217,11 +220,26 @@ int* read_rule(char **argv){
     return rule;
 }
 
-int main(int argc, char **argv){
-    int width;
-    int height;
+void save_in_file(int height, int width, FILE *fwrite, char *map){
     int i = 0;
     int j = 0;
+    for(i = 0; i < height; i++){
+        for(j = 0; j < width; j++){
+            if(map[j+(i*width)] == 0){
+                fprintf(fwrite, " ");
+            } else{
+                fprintf(fwrite, "%c", map[j+(i*width)]+'0');
+            }
+        }
+        fprintf(fwrite, "\n");
+    }
+        fclose(fwrite);
+}
+
+int main(int argc, char **argv){
+    int width = 0;
+    int height = 0;
+    printf("q for exit\n");
     if(argc < 3 || argc == 4){
             printf("Need input files\n");
             return 0;
@@ -230,46 +248,47 @@ int main(int argc, char **argv){
     char *map = read_rle(&width, &height, argv);
     if(rule == 0 || map == 0){
         printf("Error input filename\n");
+        free(rule);
+        free(map);
+        return 0;
+    }
+    if(map == (char*)1){
+        printf("Error input rle file\n");
+        free(rule);
         return 0;
     }
     if(argc == 3){
-        while(getchar() != 'k'){
-            #ifdef _WIN32
+        while(getchar() != 'q'){
+#ifdef _WIN32
             system("cls");
-            #else
+#else
             system ("clear");
-            #endif
+#endif
             draw(map, width, height);
             turn(&map,width,height,rule);
         }
-        free(rule);
-        free(map);
-        #ifdef _WIN32
+#ifdef _WIN32
+        system("cls");
         system("color 0f");
-        #endif
-    }else{
+#endif
+    } else {
         int count  = atoi(argv[4]);
         FILE * fwrite = fopen(argv[3],"w");
         if(count == 0 || fwrite == 0){
-            printf("Error input parametr\n");
+            printf("Error input filename\n");
+            free(rule);
+            free(map);
             return 0;
         }
+        int i = 0;
         for(i = 0; i < count; i++){
             turn(&map,width,height,rule);   
         }
-        for(i = 0; i < height; i++){
-            for(j = 0; j < width; j++){
-                if(map[j+(i*width)] == 0){
-                    fprintf(fwrite, " ");
-                }else{
-                    fprintf(fwrite, "%c", map[j+(i*width)]+'0');
-                }
-            }
-            fprintf(fwrite, "\n");
-        }
-        fclose(fwrite);
-        free(rule);
-        free(map);
+        save_in_file(height, width, fwrite, map);
+        system(argv[3]);
+        
     }
+    free(rule);
+    free(map);
     return 0;
 }
