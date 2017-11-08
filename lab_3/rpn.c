62f9eac43dd13883d37b93e3ef972175ac02f8e1
3000cfyz <3000cfyz@gmail.com>
2017-11-08 19:52:18 +0700

    df

--- lab_3/rpn.c
+++ lab_3/rpn.c
@@ -83,13 +83,12 @@ struct Lexem_list* getLexemList(){
     int dot = 0;
     int count_after_dot = 0;
     double num = -1;
-    while(tec != '!'){
+    while(tec != '\n'){
         scanf("%c", &tec);
         if(isdigit(tec) || tec == '.'){
             if(tec == '.'){
                 dot = 1;
-            } 
-            else{
+            } else{
                 if(num == -1){
                     num = 0;
                 }
@@ -99,8 +98,7 @@ struct Lexem_list* getLexemList(){
                     count_after_dot++;
                 }
             }
-        }
-        else{
+        } else{
             if(num != -1){
                 for(i = 0; i < count_after_dot; i++){
                     num /=10;
@@ -127,40 +125,35 @@ struct Lexem_list* getLexemList(){
                 }else{
                     push(&list,lex);
                 }
-            }else
-            if(tec == '-'){
+            }else if(tec == '-'){
                 lex.type = OPERATION;
                 lex.value.op = OP_minus;
                 lex.priority = 3;
                 op_count++;
                 if(num_count < op_count){
                     unar_op = -1;
-                }else{
+                } else{
                     push(&list,lex);
                 }
-            }else
-            if(tec == '*'){
+            }else if(tec == '*'){
                 lex.type = OPERATION;
                 lex.value.op = OP_multi;
                 lex.priority = 4;
                 push(&list,lex);
                 op_count++;
-            }else
-            if(tec == '/'){
+            }else if(tec == '/'){
                 lex.type = OPERATION;
                 lex.value.op = OP_div;
                 lex.priority = 4;
                 push(&list,lex);
                 op_count++;
-            }else
-            if(tec == '('){
+            }else if(tec == '('){
                 bracket_count++;
                 lex.type = BRACKET;
                 lex.value.br = Bracket_open;
                 lex.priority = 2;
                 push(&list,lex);
-            }else
-            if(tec == ')'){
+            }else if(tec == ')'){
                 bracket_count--;
                 lex.type = BRACKET;
                 lex.value.br = Bracket_close;
@@ -282,20 +275,21 @@ struct calc_answer calc(struct Lexem_list * rpn){
         if(l.type == OPERATION){
             if(stack->lex.type != Last){
                 right = pop(&stack);
-            }
-            else{
+            } else{
                 c_a.error = 3;
+                free(stack);
                 return c_a;
             }
             if(stack->lex.type != Last){
                 left = pop(&stack);
-            }
-            else{
+            } else{
                 c_a.error = 3;
+                free(stack);
                 return c_a;
             }
             if(right.type != NUMBER || left.type != NUMBER){
                 c_a.error = 1;
+                free(stack);
                 return c_a;
             }
             if(l.value.op == OP_minus){
@@ -313,6 +307,7 @@ struct calc_answer calc(struct Lexem_list * rpn){
             if(l.value.op == OP_div){
                 if(right.value.num_value == 0.0){
                     c_a.error = 2;
+                    free(stack);
                     return c_a;
                 }
                 right.value.num_value = left.value.num_value / right.value.num_value;
@@ -337,12 +332,11 @@ int main(){
         struct calc_answer ans = calc(rpn);
         if(ans.error == 0){
             printf("ANSWER : %f\n", ans.res);
-        }
-        else{
+        } else{
             switch(ans.error){
                 case 1: printf("no operator\n"); break;
                 case 2: printf("div on 0\n"); break;
-                case 3: printf("ono\n"); break;
+                case 3: printf("cant read operator\n"); break;
             }
             return 0;
         }
