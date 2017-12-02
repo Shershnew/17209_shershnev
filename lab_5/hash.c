9bb14ef37960a396336c26cabddfa1bc107be0fb
3000cfyz <3000cfyz@gmail.com>
2017-12-02 15:20:00 +0700

    hash optimizate

--- lab_5/hash.c
+++ lab_5/hash.c
@@ -4,11 +4,12 @@
 #include <time.h>
 
 #define NAMESIZE 50
-#define BUFSIZE 100000
+#define BUFSIZE 3
 
 struct data_base{
 	struct data ** Htable;
 	long long buf_size;
+	int size;
 };
 
 struct data{
@@ -16,8 +17,6 @@ struct data{
 	int d1;
 	int d2;
 };
-
-void insert(struct data_base * db, struct data person);
 
 unsigned int h1(char * str){
 	unsigned int hashAddress = 17;
@@ -35,16 +34,32 @@ unsigned int h2(char * str){
 	return hashAddress;
 }
 
+void insert_for_resize(struct data_base * db, struct data * person){
+	int k = 0;
+	int key = 0;
+	while(1){
+		key = (h2(person->name) + k * h1(person->name)) % db->buf_size;
+		if(db->Htable[key] == 0){
+			db->Htable[key] = person;
+			db->size++;
+			break;
+		} else {
+			k++;
+		} 
+	}
+}
+
+static int c = 0;
 void resize(struct data_base ** db){
+	printf("resize = %d\n", c++);
 	struct data_base * db_new = (struct data_base *)calloc(1, sizeof(struct data_base));
 	long long buf_size_new = (*db)->buf_size * 2;
 	db_new->Htable = (struct data **)calloc(buf_size_new, sizeof(struct data *));
 	db_new->buf_size = buf_size_new;
 	for (long long i = 0; i < (*db)->buf_size; ++i){
 		if((*db)->Htable[i] != 0){
-			insert(db_new, *((*db)->Htable[i]));
+			insert_for_resize(db_new, (*db)->Htable[i]);
 		}
-		free((*db)->Htable[i]);
 	}
 	(*db)->Htable = db_new->Htable;
 	(*db)->buf_size = db_new->buf_size;
@@ -54,6 +69,11 @@ void resize(struct data_base ** db){
 void insert(struct data_base * db, struct data person){
 	int k = 0;
 	int key = 0;
+
+	if (db->size >= db->buf_size/2) {
+		resize(&db);
+	}
+
 	while(1){
 		key = (h2(person.name) + k * h1(person.name)) % db->buf_size;
 		if(db->Htable[key] == 0){
@@ -61,13 +81,11 @@ void insert(struct data_base * db, struct data person){
 			db->Htable[key]->d1 = person.d1;
 			db->Htable[key]->d2 = person.d2;
 			memcpy(db->Htable[key]->name, person.name, NAMESIZE);
+			db->size++;
 			break;
-		} else if(k < db->buf_size){
+		} else {
 			k++;
-		} else{
-			resize(&db);
-			insert(db, person);
-		}
+		} 
 	}
 }
 
@@ -78,6 +96,7 @@ struct data_base * read(FILE * f){
 		return db;
 	}
 	db->buf_size = BUFSIZE;
+	db->size = 0;
 	db->Htable = (struct data **)calloc(db->buf_size, sizeof(struct data *));
 	struct data person;
 	while(1){
