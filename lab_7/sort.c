a4d4bb3469bf392f366adc9a5ba0dcd69a85d830
3000cfyz <3000cfyz@gmail.com>
2017-12-06 12:23:24 +0700

    fix 7

--- lab_7/sort.c
+++ lab_7/sort.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define size_for_qsort 10863
+#define size_for_qsort 20000
 
 struct file_list{
 	char name[12];
@@ -58,20 +58,17 @@ void read(struct file_list ** head, FILE * f, int count){
 	int * arr = (int *)malloc(size_for_qsort * sizeof(int));
 	int i = 0;
 	int stop_read = 0;
-	int back = 0;
 	for (i = 0; i < size_for_qsort; ++i){
 		if(-1 == fscanf(f, "%d", arr + i)){
 			last = i-1;
 			stop_read = 1;
-			back = -1;
 			break;
 		}
 	}
-	if(0 == last){
-		last = i;
+	if(!stop_read){
+		last = size_for_qsort - 1;
 	}
 	qsortt(arr, 0, last, &swap, &cmp);
-	printf("%d--\n", count);
 	for (int j = 0; j < 8; ++j){
 		(*head)->name[j] = "sortfile"[j];
 	}
@@ -80,7 +77,6 @@ void read(struct file_list ** head, FILE * f, int count){
 	(*head)->name[10] = '0'+ (count)%10;
 	(*head)->name[11] = '\0';
 	FILE * fpr = fopen((*head)->name, "w");
-	printf("%s %d\n", (*head)->name, fpr);
 	for (i = 0; i <= last; ++i){
 		fprintf(fpr, "%d ", arr[i]);
 	}
@@ -92,27 +88,31 @@ void read(struct file_list ** head, FILE * f, int count){
 }
 
 void merge(struct file_list ** head, int count){
+	if(0 == *head){
+		return;
+	}
 	if(0 == (*head)->next){
 		return;
 	}
 	struct file_list * p1 = *head;
 	struct file_list * p2 = (*head)->next;
+	printf("%d\n", count);
 	FILE * f1 = fopen(p1->name, "r");
 	FILE * f2 = fopen(p2->name, "r");
 	char rezname[12];
 	memcpy(rezname, p1->name, 12);
-	rezname[0] = '0'+count%10;
+	rezname[2] = '0'+count%10;
 	rezname[1] = '0'+(count/10)%10;
-	rezname[2] = '0'+(count/100)%10;
+	rezname[0] = '0'+(count/100)%10;
 	FILE * result = fopen(rezname, "w");
 	int a = 0;
 	int b = 0;
-	fscanf(f1, "%d", &a);
-	fscanf(f2, "%d", &b);
 	int err1 = 0;
 	int err2 = 0;
+	err1 = fscanf(f1, "%d", &a);
+	err2 = fscanf(f2, "%d", &b);
 	while(err1 != -1 && err2 != -1){
-		if(a < b){
+		if(a <= b){
 			fprintf(result, "%d ", a);
 			err1 = fscanf(f1, "%d", &a);
 		} else{
@@ -120,14 +120,13 @@ void merge(struct file_list ** head, int count){
 			err2 = fscanf(f2, "%d", &b);
 		}
 	}
-	if(err1 != -1){
-		while(-1 != fscanf(f1, "%d", &a)){
-			fprintf(result, "%d ", a);
-		}
-	} else{
-		while(-1 != fscanf(f2, "%d", &b)){
-			fprintf(result, "%d ", b);
-		}
+	while(-1 != err1){
+		fprintf(result, "%d ", a);
+		err1 = fscanf(f1, "%d", &a);
+	}
+	while(-1 != err2){
+		fprintf(result, "%d ", b);
+		err2 = fscanf(f2, "%d", &b);
 	}
 	fclose(f1);
 	fclose(f2);
@@ -137,17 +136,19 @@ void merge(struct file_list ** head, int count){
 	memcpy(p1->name, rezname, 12);
 	p1->next = p2->next;
 	free(p2);
-	printf("ppp\n");
-	merge(head, count+1);
+	printf("%s %s\n", (*head)->name, (*head)->next->name);
+	merge(&((*head)->next), count + 1);
 }
 
 void merge_sortt(FILE * f,
  void (*read)(struct file_list ** head, FILE * f, int count)){
 	struct file_list * file_base = 0;
 	read(&file_base, f, 0);
-	printf("-----------------------------------\n");
-	merge(&file_base, 0);
-	printf("+++++++++++++++++++++++++++++++++++\n");
+	int k = 0;
+	while(file_base->next != 0){
+		merge(&file_base, k);
+		k += 100;
+	}
 }
 
 int main(int argc, char **argv){
@@ -156,6 +157,9 @@ int main(int argc, char **argv){
 		printf("error read\n");
 		return -1;
 	}
+	clock_t start = clock();
 	merge_sortt(f, &read);
+	clock_t finish = clock();
+	printf("Read %f seconds\n", ((float)(finish - start)) / CLOCKS_PER_SEC);
 	return 0;
 }
