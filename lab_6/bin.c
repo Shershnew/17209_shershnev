26a16bbdc0a0654cb7ef32b41cda83993a1ae974
3000cfyz <3000cfyz@gmail.com>
2017-12-12 21:39:13 +0700

    addlab_6

--- lab_6/bin.c
+++ lab_6/bin.c
@@ -1,18 +1,74 @@
 #include <stdio.h>
 
-int main(){
-	float f;
-	scanf("%f", &f);
-	unsigned char * p = (unsigned char *)&f;
-	char str[33];
-	str[33] = '\0';
-	for (int j = 0; j < 4; ++j){
-		for (int i = 0; i < 8; ++i){
-			str[31-i-(8*j)] ='0' + *p % 2;
-			*p = *p>>1;
-		}
-		p++;
+double poww(int b){
+	double r = 1;
+	for (int i = 0; i < b; ++i){
+		r /= 2.0;
 	}
-	printf("%s\n", str);
+	return r;
+}
+
+int main(){
+	double f;
+	unsigned long long sign = 0;
+	unsigned long long exp = 0;
+	double fraction = 1.0;
+	scanf("%lf", &f);
+	unsigned char * p = (unsigned char *)&f;
+	sign = *(p+7) >> 7;
+	printf("sign - %d\n", sign);
+
+
+	for (int i = 1; i < 8; ++i){
+		exp = exp << 1;
+		exp += (*(p + 7) >> 7 - i) % 2;
+	}
+	for (int i = 0; i < 4; ++i){
+		exp = exp << 1;
+		exp += (*(p + 6) >> 7 - i) % 2;
+	}
+	printf("exp - %d\n", exp);
+
+
+	for (int i = 4; i < 8; ++i){
+		fraction += ((*(p + 6) >> 7 - i) % 2) * poww(i - 3);
+	}
+	for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 5) >> 7 - i) % 2) * poww(i + 5);
+	}
+	for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 4) >> 7 - i) % 2) * poww(i + 13);
+	}
+		for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 3) >> 7 - i) % 2) * poww(i + 21);
+	}
+		for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 2) >> 7 - i) % 2) * poww(i + 29);
+	}
+		for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 1) >> 7 - i) % 2) * poww(i + 37);
+	}
+		for (int i = 0; i < 8; ++i){
+		fraction += ((*(p + 0) >> 7 - i) % 2) * poww(i + 45);
+	}
+	printf("fraction - %.10lf\n", fraction);
+
+//bin
+	printf("%d  ", sign);
+	for (int i = 1; i < 8; ++i){
+		printf("%d", (*(p + 7) >> (7 - i)) % 2);
+	}
+	for (int i = 0; i < 4; ++i){
+		printf("%d", (*(p + 6) >> 7 - i) % 2);
+	}
+	printf("  ");
+	for (int i = 4; i < 8; ++i){
+		printf("%d", (*(p + 6) >> 7 - i) % 2);
+	}
+	for (int j = 0; j < 6; ++j){
+		for (int i = 0; i < 8; ++i){
+			printf("%d", (*(p + 5 - j) >> 7 - i) % 2);
+		}
+	}
 	return 0;
 }
